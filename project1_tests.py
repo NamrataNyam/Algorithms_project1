51e2a7d001b1bfadb64be52a73533820ac006e40
Shubham Sharma <64421924+Shubhamm097@users.noreply.github.com>
2024-04-21 18:32:53 -0700

    Update project1_tests.py

--- project1_tests.py
+++ project1_tests.py
@@ -1,8 +1,8 @@
 import sorting as ref_sorting 
 import random
 import time
+import math
 import matplotlib.pyplot as plt
-
 
 '''
 The first part of this code will verify functional correctness of all sorting algorithms.
@@ -19,12 +19,13 @@ sorter[1] = ref_sorting.QuickSort()
 sorter[2] = ref_sorting.InsertionSort()
 sorter[3] = ref_sorting.ShellSort([7,3,1])
 sorter[4] = ref_sorting.ShellSort([1000,100,10,1])
-sorter[5] = ref_sorting.BucketSort(1000)
+sorter[5] = ref_sorting.BucketSort(16000)
 sorter[6] = ref_sorting.RadixSort()
 
 DATA_SIZE = 500
 NUM_EXP = 5
 data = []
+almost_sorted = []
 
 uniformDistributedTimes = [[] for _ in range(7)]
 almostSortedTimes = [[] for _ in range(7)]
@@ -47,18 +48,35 @@ for i in range(len(sorter)):
     sorter[i].sort(data)
     verify(data,test_data,i)
 
-# Uniformly Distributed Permutations
+
 for j in range(NUM_EXP):
     print()
     DATA_SIZE = DATA_SIZE * 2
     print("DATA_SIZE:", DATA_SIZE)
     data = []
+    almost_sorted = []
     for i in range(DATA_SIZE):
-        data.append(random.randint(0,1000))
+        data.append(random.randint(0,DATA_SIZE))
+        # data.append(random.randint(0,1000))
+
+    # Uniformly distributed data
+    for i in range(DATA_SIZE):
+        almost_sorted.append(i)
+
+    # Almost sorted data
+    pairs = 2*int(math.log(DATA_SIZE,2))
+    # print("num pairs:", pairs)
+    for i in range(pairs):
+        idx1 = random.randint(0,DATA_SIZE)
+        idx2 = random.randint(0,DATA_SIZE)
+        temp = almost_sorted[idx1]
+        almost_sorted[idx1] = almost_sorted[idx2]
+        almost_sorted[idx2] = temp
        
     data_save = data.copy()
     test_data = sorted(data)
 
+    # Uniformly distributed data
     for i in range(len(sorter)):
 
         data = data_save.copy()
@@ -66,9 +84,29 @@ for j in range(NUM_EXP):
         sorter[i].sort(data)
         end_time = time.perf_counter()
         sorter[i].time = end_time - start_time
-
+    print()
+    print("Uniformly Distributed Random Data: ")
     for i in range(len(sorter)):
         uniformDistributedTimes[i].append(sorter[i].time)
+        print(names[i], sorter[i].time)
+
+    # Almost sorted data
+    for i in range(len(sorter)):
+
+        data = almost_sorted.copy()
+        start_time = time.perf_counter()
+        if i != 5:
+            sorter[i].sort(data)
+        else:
+            sorter[i].range = DATA_SIZE
+            sorter[i].sort(data)
+        end_time = time.perf_counter()
+        sorter[i].time = end_time - start_time
+    
+    print()
+    print("Almost Sorted Data: ")
+    for i in range(len(sorter)):
+        almostSortedTimes[i].append(sorter[i].time)
         print(names[i], sorter[i].time)
 
 DATA_SIZE=500
@@ -87,43 +125,6 @@ plt.ylabel('Time (s)')
 plt.title('Sorting Time Comparison')
 plt.legend()
 plt.show()
-
-# Almost Sorted Data
-DATA_SIZE = 500
-NUM_EXP = 5
-data = []
-
-for j in range(NUM_EXP):
-    print()
-    DATA_SIZE = DATA_SIZE * 2
-    print("DATA_SIZE:", DATA_SIZE)
-    data = []
-    for i in range(DATA_SIZE):
-        data.append(random.randint(0,1000))
-
-    data.sort()
-    i, j = 0, len(data)-1
-
-    while i < j:
-        data[i], data[j] = data[j], data[i]
-        i += 1
-        j -= 1
-
-       
-    data_save = data.copy()
-    test_data = sorted(data)
-
-    for i in range(len(sorter)):
-
-        data = data_save.copy()
-        start_time = time.perf_counter()
-        sorter[i].sort(data)
-        end_time = time.perf_counter()
-        sorter[i].time = end_time - start_time
-
-    for i in range(len(sorter)):
-        almostSortedTimes[i].append(sorter[i].time)
-        print(names[i], sorter[i].time)
 
 DATA_SIZE=500
 data_sizes = [DATA_SIZE * (2 ** n) for n in range(NUM_EXP)]
